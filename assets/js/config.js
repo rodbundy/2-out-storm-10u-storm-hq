eeac32bc5db07dda323feeeac456f66cced5ecdc
rodbundy <sr.rodbundy@gmail.com>
2026-08-17 09:24:39 -0400

    Fix Firebase API key

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,21 @@
-window.STORM_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbzD520mV0DjuluW6YxyIuNR4dGnKp85wYNEBmbywGQT-CAtoImbAX5gBYzoKjQ-v77uvg/exec",
-  fallbackLogo: "assets/img/storm-logo.svg",
-  domain: "2outstorm2035.com"
+// STORM HQ PUSH CONFIG
+// Public Firebase web config + public VAPID key only. NEVER put a service-account private key here.
+// Tomorrow: replace the placeholders, then change enabled to true.
+globalThis.STORM_PUSH_CONFIG = {
+  enabled: true,
+  sdkVersion: '12.17.1',
+  firebase: {
+    apiKey: 'AIzaSyCel5T-okqbTOWbzrG3-eqHjrT3hC9XV2c',
+    authDomain: 'out-storm-hq.firebaseapp.com',
+    projectId: 'out-storm-hq',
+    storageBucket: 'out-storm-hq.firebasestorage.app',
+    messagingSenderId: '461935348525',
+    appId: '1:461935348525:web:936885a34a2c943164e1a4'
+  },
+  vapidKey: 'BEv1StxuIERsFkFD5s7ue3Q8_8bsORZJh-iM1pbe7rytEsV72PvhJgBCLIDkVOWi8Q-lS4E1CdOs50Mxof2dH00',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzD520mV0DjuluW6YxyIuNR4dGnKp85wYNEBmbywGQT-CAtoImbAX5gBYzoKjQ-v77uvg/exec',
+  appName: '2 Out Storm 10U',
+  notificationIcon: '/assets/img/icons/icon-192.png',
+  notificationBadge: '/assets/img/icons/badge-96.png',
+  defaultUrl: '/'
 };
